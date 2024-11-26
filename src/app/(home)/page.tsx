4c1b245072335a85214569430106c3b09049966d
AntonioSGJR <antoniosaraiva.gois@gmail.com>
2024-11-26 14:09:49 -0300

    Mudança na organização das pastas

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import Header from "./_components/header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">
+
+      </main>
+    </>
+  );
+}
